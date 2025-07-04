209b12da2cbc5673416f1e935585e440b3237569
Pedro Henrique Rodrigues Soares <pedro.henriquers@outlook.com>
2025-07-03 21:13:57 -0300

    Update startup.php

--- system/startup.php
+++ system/startup.php
@@ -8,7 +8,7 @@ if (version_compare(phpversion(), '5.6.0', '<') == true) {
 }
 
 if (!ini_get('date.timezone')) {
-	date_default_timezone_set('UTC');
+	date_default_timezone_set('America/Sao_Paulo');
 }
 
 // Windows IIS Compatibility
