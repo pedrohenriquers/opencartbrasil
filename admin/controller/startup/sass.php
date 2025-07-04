8179f4c631d9fb6b2bf3984e3e4e22435309c865
Pedro Henrique Rodrigues Soares <pedro.henriquers@outlook.com>
2025-07-04 15:32:16 -0300

    desabilitado cache devido problemas locaweb

--- admin/controller/startup/sass.php
+++ admin/controller/startup/sass.php
@@ -3,6 +3,8 @@ class ControllerStartupSass extends Controller {
 	public function index() {
 		$file = DIR_APPLICATION . 'view/stylesheet/bootstrap.css';
 
+		// COMENTE TODO ESTE BLOCO PARA DESABILITAR A COMPILAÇÃO DO BOOTSTRAP.SCSS
+		/*
 		if (!is_file($file) || !$this->config->get('developer_sass')) {
 			$scss = new \Leafo\ScssPhp\Compiler();
 			$scss->setImportPaths(DIR_APPLICATION . 'view/stylesheet/sass/');
@@ -21,5 +23,6 @@ class ControllerStartupSass extends Controller {
 
 			fclose($handle);
 		}
+		*/
 	}
 }
--- catalog/controller/startup/sass.php
+++ catalog/controller/startup/sass.php
@@ -3,6 +3,8 @@ class ControllerStartupSass extends Controller {
 	public function index() {
 		$file = DIR_APPLICATION . 'view/theme/' . $this->config->get('theme_directory') . '/stylesheet/bootstrap.css';
 
+		// COMENTE TODO ESTE BLOCO PARA DESABILITAR A COMPILAÇÃO DO BOOTSTRAP.SCSS
+		/*
 		if (!is_file($file) || (is_file(DIR_APPLICATION . 'view/theme/' . $this->config->get('theme_directory') . '/stylesheet/sass/bootstrap.scss') && !$this->config->get('developer_sass'))) {
 			$scss = new \Leafo\ScssPhp\Compiler();
 			$scss->setImportPaths(DIR_APPLICATION . 'view/theme/' . $this->config->get('theme_directory') . '/stylesheet/sass/');
@@ -22,23 +24,26 @@ class ControllerStartupSass extends Controller {
 
 			fclose($handle);
 		}
+		*/
 
 		$file = DIR_APPLICATION . 'view/theme/' . $this->config->get('theme_directory') . '/stylesheet/stylesheet.css';
 
+		// COMENTE TODO ESTE SEGUNDO BLOCO PARA DESABILITAR A COMPILAÇÃO DO _STYLESHEET.SCSS
+		/*
 		if (!is_file($file) || (is_file(DIR_APPLICATION . 'view/theme/' . $this->config->get('theme_directory') . '/stylesheet/_stylesheet.scss') && !$this->config->get('developer_sass'))) {
-			include_once(DIR_STORAGE . 'vendor/leafo/scssphp/scss.inc.php');
+			include_once(DIR_STORAGE . 'vendor/leafo/scssphp/scss.inc.php'); // Esta linha também será comentada
 
 			$scss = new \Leafo\ScssPhp\Compiler();
 			$scss->setImportPaths(DIR_APPLICATION . 'view/theme/' . $this->config->get('theme_directory') . '/stylesheet/');
 
 			$output = $scss->compile('@import "_stylesheet.scss"');
 
-			$output = preg_replace('/\s*{\s*/', ' {' . "\n" . '    ', $output);
-			$output = preg_replace('/;\s*/', ';' . "\n" . '    ', $output);
+			$output = preg_replace('/\s*{\s*/', ' {' . "\n" . '    ', $output);
+			$output = preg_replace('/;\s*/', ';' . "\n" . '    ', $output);
 			$output = preg_replace('/,\s*/', ', ', $output);
 			$output = preg_replace('/[ ]*}\s*/', '}' . "\n", $output);
 			$output = preg_replace('/\}\s*(.+)/', '}' . "\n" . '$1', $output);
-			$output = preg_replace('/\n    ([^:]+):\s*/', "\n" . '    $1: ', $output);
+			$output = preg_replace('/\n    ([^:]+):\s*/', "\n" . '    $1: ', $output);
 			$output = preg_replace('/([A-z0-9\)])}/', '$1;' . "\n" . '}', $output);
 
 			$handle = fopen($file, 'w');
@@ -53,5 +58,6 @@ class ControllerStartupSass extends Controller {
 
 			fclose($handle);
 		}
+		*/
 	}
 }
